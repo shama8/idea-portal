e71adaf008bf76c10fba4e0c1284f749901d5d52
Shama M <shama.m@aventragroup.com>
2025-09-04 18:04:40 +0530

    Resolving bugs

--- backend/app.py
+++ backend/app.py
@@ -152,4 +152,5 @@ def get_all_ideas():
 # Run the Flask App
 # --------------------------------------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 locally
+    app.run(host='0.0.0.0', port=port, debug=True)
