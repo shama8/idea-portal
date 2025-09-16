cbda75e0135520cecf12937fcf3964803743afb7
Shama M <shama.m@aventragroup.com>
2025-09-16 11:02:52 +0530

    Similarity 4

--- backend/app.py
+++ backend/app.py
@@ -210,4 +210,4 @@ def clear_ideas():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
